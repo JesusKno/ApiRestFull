8cb0beac1bfdab240e67e28914e89816adbaa1e3
jkno96 <jcano1460@gmail.com>
2022-07-07 00:00:33 -0500

    added function to add product

--- server.js
+++ server.js
@@ -5,48 +5,22 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 /**Router */
-
+const productos = []
 routerApi = Router()
 app.use('/api', routerApi);
-
-const productos = [
-    {
-        "id": 1,
-        "title": "Producto 1",
-        "price": 10,
-        "thumbnail": "www.algo.algo"
-    }
-];
-
 routerApi.get('/productos', (req, res) => {
-    res.json(productos);
+    res.send(productos)
 });
 
 routerApi.post('/productos', (req, res) => {
-    productos.push(req.body);
-    res.json(productos)
-})
+    const response = addProduct(req.body);
+    res.json(response)
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+routerApi.put('/update/productos', (req, res) => {
+    const response = updateProduct(req.body);
+    res.json(response)
+});
 
 /**Server port */
 const PORT = 8080;
@@ -54,3 +28,34 @@ const server = app.listen(PORT, ()=> {
     console.log(`Servidor escuchando en el puerto: ${PORT}`);
 });
 server.on('error', error => console.log(`Errro en servidor ${error}`));
+
+/**Functions */
+
+function addProduct(product) {
+    if (product) {
+        const id = productos.length + 1;
+        productos.push({
+            "id": id,
+            "title": product.title,
+            "price": product.price,
+            "thumbnail": product.thumbnail
+        });
+
+        return productos
+    }
+}
+
+function updateProduct(product) {
+    if (product) {
+        const productId = product.map((productId) = productId.id);
+        const index = productos.findIndex(id => id === productId)
+        productos[index].push({
+            "id": id,
+            "title": product.title,
+            "price": product.price,
+            "thumbnail": product.thumbnail
+        });
+
+        return productos[index]
+    }
+}
